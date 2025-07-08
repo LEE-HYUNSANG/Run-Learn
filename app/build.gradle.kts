2f373b2cc1272f48d0d22501a11dbe779a5bb600
LEE-HYUNSANG <131837885+LEE-HYUNSANG@users.noreply.github.com>
2025-07-09 03:02:37 +0900

    Fix Hilt plugin id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("kapt")
-    id("com.google.dagger.hilt.android")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
