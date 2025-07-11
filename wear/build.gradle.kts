8ce48c5601369fb9e3819f1a1ebcba0f9498d5bd
LEE-HYUNSANG <131837885+LEE-HYUNSANG@users.noreply.github.com>
2025-07-11 12:33:54 +0900

    Add Material library to resolve theme

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:1.6.6")
     implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
 
     implementation("com.google.android.horologist:horologist-compose-layout:0.5.17")
